252accf7efe064bcd3f0a2abe591cea155a23ad7
Zowhy <dtrovato892@gmail.com>
2024-03-20 16:48:21 -0400

    link banco de dados

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,22 @@
-
 export default async function Home() {
-  return (
-    <main>
-      <h1>Home</h1>
-    </main>
-  )
+  const resposta = await fetch("http://back-end-ifms.vercel.app/campi",{
+    next:{
+    revalidate: 1
+  }
+});
+const campus = await resposta.json();
+    return (
+      <main>
+        <h1>Home</h1>
+        {
+          campus.map((campi) =>
+            <div>
+              <p>{campi.nome_campi}</p>
+            </div>
+
+
+          )
+      }
+      </main>
+    )
 }
