2b2b09ca290bdff30fcebdc14c0f31e1313d0fc3
Zowhy <dtrovato892@gmail.com>
2024-03-20 17:27:44 -0400

    campus

--- src/app/page.js
+++ src/app/page.js
@@ -4,14 +4,14 @@ export default async function Home() {
     revalidate: 1
   }
 });
-const campus = await resposta.json();
+const campi = await resposta.json();
     return (
       <main>
         <h1>Home</h1>
         {
-          campus.map((campi) =>
+          campi.map((campus) =>
             <div>
-              <p>{campi.nome_campi}</p>
+              <p>{campus.nome_campus}</p>
             </div>
 
 
